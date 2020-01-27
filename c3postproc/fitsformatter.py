f38072b252b5ba192b36334a293d2dc8f150a3ce
Trygve Leithe Svalheim <trygvels@astro.uio.no>
2020-01-27 13:27:18 +0100

    Supports LFI sky maps

--- c3postproc/fitsformatter.py
+++ c3postproc/fitsformatter.py
@@ -6,16 +6,16 @@ import healpy as hp
 import fitsio
 from c3postproc.tools import *
 
-def format_fits(chain, extname, types, units, nside, burnin, polar, component, fwhm, nu_ref, procver, filename,):
+def format_fits(chain, extname, types, units, nside, burnin, polar, component, fwhm, nu_ref, procver, filename, bndctr, restfreq, bndwid,):
     print()
     print(f"Formatting and outputting {filename}")
-    header = get_header(extname, types, units, nside, polar, component, fwhm, nu_ref, procver, filename)
-    dset = get_data(chain, extname, burnin, fwhm, nside, types)    
+    header = get_header(extname, types, units, nside, polar, component, fwhm, nu_ref, procver, filename, bndctr, restfreq, bndwid)
+    dset = get_data(chain, extname, component, burnin, fwhm, nside, types)    
 
     print(f"{procver}/{filename}", dset.shape)
     hp.write_map(f"{procver}/{filename}",dset,column_names=types, column_units=units, coord='G', overwrite=True, extra_header=header)
 
-def get_data(chain, extname, burnin, fwhm, nside, types):
+def get_data(chain, extname, component, burnin, fwhm, nside, types):
     if extname.endswith('CMB'):
         # Mean data
         amp_mean = h5handler(input=chain, dataset="cmb/amp_map", min=burnin, max=None, output="map",fwhm=fwhm,  nside=nside, command=np.mean)
@@ -98,24 +98,49 @@ def get_data(chain, extname, burnin, fwhm, nside, types):
         
         dset[2] = amp_stddev[0]
         dset[3] = nu_p_stddev[0]
+    if extname.endswith('FREQMAP'):
+                # Mean data
+        amp_mean = h5handler(input=chain, dataset=f"tod/{component}/map", min=burnin, max=None, output="map",fwhm=fwhm,  nside=nside, command=np.mean)
+
+        # stddev data
+        amp_stddev = h5handler(input=chain, dataset=f"tod/{component}/map", min=burnin, max=None, output="map",fwhm=fwhm, nside= nside,command= np.std)
+
+        # Masks
+
+        dset = np.zeros((len(types),hp.nside2npix(nside)))
+
+        dset[0] = amp_mean[0,:]
+        dset[1] = amp_mean[1,:]
+        dset[2] = amp_mean[2,:]
+        
+        dset[3] = amp_stddev[0,:]
+        dset[4] = amp_stddev[1,:]
+        dset[5] = amp_stddev[2,:]
 
     print(f"Shape of dset {dset.shape}")
     return dset
 
-def get_header(extname, types, units, nside, polar, component, fwhm, nu_ref, procver, filename):
-
+def get_header(extname, types, units, nside, polar, component, fwhm, nu_ref, procver, filename, bndctr, restfreq, bndwid):
     stamp = f'Written {time.strftime("%c")}'
     
     header = []
-    header.append(("PIXTYPE", "HEALPIX"))
+    header.append(("DATE", stamp, "Time and date of creation."))
+    header.append(("PIXTYPE", "HEALPIX", "HEALPIX pixelisation."))
     header.append(("COORDSYS", "GALACTIC"))
     header.append(("POLAR", polar))
-    header.append(("BAD_DATA", hp.UNSEEN))
-    header.append(("METHOD", "COMMANDER"))
+    header.append(("BAD_DATA", hp.UNSEEN, "HEALPIX UNSEEN value."))
+    header.append(("METHOD", "COMMANDER", "COMMANDER sampling framework"))
     header.append(("AST-COMP", component))
-    header.append(("FWHM", fwhm))
-    header.append(("NU_REF", nu_ref))
-    header.append(("PROCVER", procver))
+    if extname == "FREQMAP":
+        header.append(("FREQ", nu_ref))
+    else:
+        header.append(("FWHM", fwhm))
+        header.append(("NU_REF", nu_ref))
+    header.append(("PROCVER", procver, "Release version"))
     header.append(("FILENAME", filename))
-
+    if extname == "FREQMAP":
+        # TODO are these correct?
+        header.append(('BNDCTR', bndctr, "Formal Band Center"))
+        header.append(('RESTFREQ', restfreq, "Effective Central Frequency"))
+        header.append(('BNDWID',bndwid, "Effective Bandwidth"))
     return header
