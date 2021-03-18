8c1b0d33284e5dd04f4bf7bfdc6c0931abc85426
Trygve Leithe Svalheim <trygvels@astro.uio.no>
2021-03-18 16:28:04 +0100

    Fixed labeling errors and dimension bug when passing list2

--- src/commands_plotting.py
+++ src/commands_plotting.py
@@ -1051,13 +1051,11 @@ def make_diff_plots(ctx, dir1, dir2, type1, type2):
   
         from src.plotter import trygveplot
  
-        units = None
         size = 'm'
         if comp in ['030', '044', '070']:
-            units = '$\mu\mathrm{K}_{\mathrm{CMB}}$'
             size = 's'
 
-        trygveplot(diff_map, outname=comp + '_diff' + '.pdf', auto=True, rng='auto', colorbar=True, size=size, unit=units, )
+        trygveplot(diff_map, outname=comp + '_diff' + '.pdf', auto=True, rng='auto', colorbar=True, size=size,)
 
 @commands_plotting.command()
 @click.option("-pol", is_flag=True, help="",)
