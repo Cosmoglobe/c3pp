low memory usage, and a fix to mean/stddev calculation for said low memory computation added to c3pp. also, in case of low memory stddev, if name of output has "stddev" in it, a mean map is also written with "stddev" excanged to "mean" in output name.

src/commands_fits.py
@@ -229,8 +229,9 @@ def crosspec(input1, input2, output, beam1, beam2, mask,):
 @click.option("-zerospin", is_flag=True, help="If smoothing, treat maps as zero-spin maps.",)
 @click.option("-missing", is_flag=True, help="If files are missing, drop them. Else, exit computation",)
 @click.option("-pixweight", default=None, type=click.STRING, help="Path to healpy pixel weights.",)
+@click.option("-lowmem", is_flag=True, help="Calculate using least amount of memory. For low RAM systems. This method is slower!",)
 def fits_mean(
-        input, output, min, max, minchain, maxchain, chaindir, fwhm, nside, zerospin, missing, pixweight):
+        input, output, min, max, minchain, maxchain, chaindir, fwhm, nside, zerospin, missing, pixweight, lowmem):
     """
     Calculates the mean over sample range from fits-files.
     ex. res_030_c0001_k000001.fits res_030_20-100_mean_40arcmin.fits -min 20 -max 100 -fwhm 40 -maxchain 3\n
@@ -239,7 +240,7 @@ def fits_mean(
     Note: the input file name must have the 'c0001' chain identifier and the 'k000001' sample identifier. The -min/-max and -chainmin/-chainmax options set the actual samples/chains to be used in the calculation 
     """
 
-    fits_handler(input, min, max, minchain, maxchain, chaindir, output, fwhm, nside, zerospin, missing, pixweight, np.mean, write=True)
+    fits_handler(input, min, max, minchain, maxchain, chaindir, output, fwhm, nside, zerospin, missing, pixweight, np.mean, lowmem=lowmem, write=True, return_data=False)
 
 @commands_fits.command()
 @click.argument("input", type=click.STRING)
@@ -254,8 +255,9 @@ def fits_mean(
 @click.option("-zerospin", is_flag=True, help="If smoothing, treat maps as zero-spin maps.",)
 @click.option("-missing", is_flag=True, help="If files are missing, drop them. Else, exit computation",)
 @click.option("-pixweight", default=None, type=click.STRING, help="Path to healpy pixel weights.",)
+@click.option("-lowmem", is_flag=True, help="Calculate using least amount of memory. For low RAM systems. This method is slower!",)
 def fits_stddev(
-        input, output, min, max, minchain, maxchain, chaindir, fwhm, nside, zerospin, missing, pixweight):
+        input, output, min, max, minchain, maxchain, chaindir, fwhm, nside, zerospin, missing, pixweight, lowmem):
     """
     Calculates the standard deviation over sample range from fits-files.
     ex. res_030_c0001_k000001.fits res_030_20-100_mean_40arcmin.fits -min 20 -max 100 -fwhm 40 -maxchain 3
@@ -264,4 +266,4 @@ def fits_stddev(
     Note: the input file name must have the 'c0001' chain identifier and the 'k000001' sample identifier. The -min/-max and -chainmin/-chainmax options set the actual samples/chains to be used in the calculation 
     """
 
-    fits_handler(input, min, max, minchain, maxchain, chaindir, output, fwhm, nside, zerospin, missing, pixweight, np.std, write=True)
+    fits_handler(input, min, max, minchain, maxchain, chaindir, output, fwhm, nside, zerospin, missing, pixweight, np.std, lowmem=lowmem, write=True, return_data=False)

src/commands_hdf.py
@@ -35,7 +35,9 @@ def split(input,dataset,output,min,max,maxchain,notchain):
 @click.option("-nside", default=None, type=click.INT, help="Nside for alm binning",)
 @click.option("-zerospin", is_flag=True, help="If smoothing, treat maps as zero-spin maps.",)
 @click.option("-pixweight", default=None, type=click.STRING, help="Path to healpy pixel weights.",)
-def mean(input, dataset, output, min, max, maxchain, fwhm, nside, zerospin, pixweight):
+@click.option("-lowmem", is_flag=True, help="Calculate using least amount of memory. For low RAM systems. This method is slower!",)
+@click.option("-chaindir", default=None, type=click.STRING, help="Base of chain directory, overwrites chain iteration from input file name to iteration over chain directories, BP_chain_c15 to BP_chain_c19 [ex. 'BP_chain', with minchain = 15 and maxchain = 19]",)
+def mean(input, dataset, output, min, max, maxchain, fwhm, nside, zerospin, pixweight, lowmem, chaindir):
     """
     Calculates the mean over sample range from .h5 file.
     ex. chains_c0001.h5 dust/amp_map 5 50 dust_5-50_mean_40arcmin.fits -fwhm 40 -maxchain 3
@@ -47,7 +49,7 @@ def mean(input, dataset, output, min, max, maxchain, fwhm, nside, zerospin, pixw
         sys.exit()
 
 
-    h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, np.mean, pixweight, zerospin,)
+    h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, np.mean, pixweight, zerospin, lowmem=lowmem, chdir=chaindir, return_data=False)
 
 @commands_hdf.command()
 @click.argument("input", type=click.STRING)
@@ -60,7 +62,9 @@ def mean(input, dataset, output, min, max, maxchain, fwhm, nside, zerospin, pixw
 @click.option("-nside", default=None, type=click.INT, help="Nside for alm binning",)
 @click.option("-zerospin", is_flag=True, help="If smoothing, treat maps as zero-spin maps.",)
 @click.option("-pixweight", default=None, type=click.STRING, help="Path to healpy pixel weights.",)
-def stddev(input, dataset, output, min, max, maxchain, fwhm, nside, zerospin, pixweight,):
+@click.option("-lowmem", is_flag=True, help="Calculate using least amount of memory. For low RAM systems. This method is slower!",)
+@click.option("-chaindir", default=None, type=click.STRING, help="Base of chain directory, overwrites chain iteration from input file name to iteration over chain directories, BP_chain_c15 to BP_chain_c19 [ex. 'BP_chain', with minchain = 15 and maxchain = 19]",)
+def stddev(input, dataset, output, min, max, maxchain, fwhm, nside, zerospin, pixweight, lowmem, chaindir):
     """
     Calculates the stddev over sample range from .h5 file.
     ex. chains_c0001.h5 dust/amp_map 5 50 dust_5-50_mean_40arcmin.fits -fwhm 40 -maxchain 3
@@ -72,7 +76,7 @@ def stddev(input, dataset, output, min, max, maxchain, fwhm, nside, zerospin, pi
         click.echo("Please specify nside when handling alms.")
         sys.exit()
 
-    h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, np.std, pixweight, zerospin,)
+    h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, np.std, pixweight, zerospin, lowmem=lowmem, chdir=chaindir, return_data=False)
 
 @commands_hdf.command()
 @click.argument("filename", type=click.STRING)

src/tools.py
@@ -66,7 +66,7 @@ def alm2fits_tool(input, dataset, nside, lmax, fwhm, save=True,):
         hp.write_map(outfile + f"_n{str(nside)}_lmax{lmax}.fits", maps, overwrite=True, dtype=None)
     return maps, nside, lmax, fwhm, outfile
 
-def h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, command, pixweight=None, zerospin=False, lowmem=False, notchain=False):
+def h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, command, pixweight=None, zerospin=False, lowmem=False, chdir=None, notchain=False, write=True, return_data=True):
     """
     Function for calculating mean and stddev of signals in hdf file
     """
@@ -76,7 +76,11 @@ def h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, command, 
     from tqdm import tqdm
 
     if (lowmem and command == np.std): #need to compute mean first
-        mean_data = h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, np.mean, pixweight, zerospin, lowmem,)
+        if ('stddev' in output):
+            temp_output=output.replace('stddev','mean')
+            mean_data = h5handler(input, dataset, min, max, maxchain, temp_output, fwhm, nside, np.mean, pixweight, zerospin, lowmem, chdir=chdir,)
+        else:
+            mean_data = h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, np.mean, pixweight, zerospin, lowmem, chdir=chdir, write=False)
 
     print()
     if command: print("{:-^50}".format(f" {dataset} calculating {command.__name__} "))
@@ -103,7 +107,10 @@ def h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, command, 
     maxnone = True if max == None else False  # set length of keys for maxchains>1
     pol = True if zerospin == False else False  # treat maps as TQU maps (polarization)
     for c in range(1, maxchain + 1):
-        filename = input.replace("c0001", "c" + str(c).zfill(4))
+        if (chdir==None):
+            filename = input.replace("c0001", "c" + str(c).zfill(4))
+        else:
+            filename = chdir+'_c%i/'%(c)+input
         with h5py.File(filename, "r") as f:
 
             if notchain:
@@ -224,20 +231,22 @@ def h5handler(input, dataset, min, max, maxchain, output, fwhm, nside, command, 
             outdata = hp.sphtfunc.smoothing(outdata, fwhm=arcmin2rad(fwhm),verbose=False,pol=pol,use_weights=True)
 
     # Outputs fits map if output name is .fits
-    if output.endswith(".fits"):
-        hp.write_map(output, outdata, overwrite=True, dtype=None)
-    elif output.endswith(".dat"):
-        while np.ndim(outdata)>2: 
-            if outdata.shape[-1]==4:
-                tdata = outdata[:,0,0]
-                print(tdata)
-                outdata = outdata[:,:,3]
-                outdata[:,0] = tdata
-            else:
-                outdata = outdata[:,:,0]
+    if (write):
+        if output.endswith(".fits"):
+            hp.write_map(output, outdata, overwrite=True, dtype=None)
+        elif output.endswith(".dat"):
+            while np.ndim(outdata)>2: 
+                if outdata.shape[-1]==4:
+                    tdata = outdata[:,0,0]
+                    print(tdata)
+                    outdata = outdata[:,:,3]
+                    outdata[:,0] = tdata
+                else:
+                    outdata = outdata[:,:,0]
 
-        np.savetxt(output, outdata)
-    return outdata
+            np.savetxt(output, outdata)
+    if return_data:
+        return outdata
 
 def arcmin2rad(arcmin):
     return arcmin * (2 * np.pi) / 21600
@@ -420,7 +429,7 @@ def rspectrum(nu, r, sig, scaling=1.0):
     A = np.sqrt(sum( 4*np.pi * cl[2:,signal]*bl[2:,signal]**2/(2*l+1) ))
     return cmb(nu, A*scaling)
 
-def fits_handler(input, min, max, minchain, maxchain, chdir, output, fwhm, nside, zerospin, drop_missing, pixweight, command, lowmem=False, fields=None, write=False):
+def fits_handler(input, min, max, minchain, maxchain, chdir, output, fwhm, nside, zerospin, drop_missing, pixweight, command, lowmem=False, fields=None, write=True, return_data=True):
     """
     Function for handling fits files.
     """
@@ -434,7 +443,11 @@ def fits_handler(input, min, max, minchain, maxchain, chdir, output, fwhm, nside
         exit()
 
     if (lowmem and command == np.std): #need to compute mean first
-        mean_data = fits_handler(input, min, max, minchain, maxchain, chdir, output, fwhm, nside, zerospin, drop_missing, pixweight, lowmem, np.mean, fields, write=False)
+        if ('stddev' in output):
+            temp_output=output.replace('stddev','mean')
+            mean_data = fits_handler(input, min, max, minchain, maxchain, chdir, temp_output, fwhm, nside, zerospin, drop_missing, pixweight, np.mean, lowmem, fields, write=True, return_data=True)
+        else:
+            mean_data = fits_handler(input, min, max, minchain, maxchain, chdir, output, fwhm, nside, zerospin, drop_missing, pixweight, np.mean, lowmem, fields, write=False, return_data=True)
 
     if (minchain > maxchain):
         print('Minimum chain number larger that maximum chain number. Exiting')
@@ -624,6 +637,6 @@ def fits_handler(input, min, max, minchain, maxchain, chdir, output, fwhm, nside
             hp.write_map(output, outdata, overwrite=True, dtype=None)
         elif output.endswith(".dat"):
             np.savetxt(output, outdata)
-    else:
+    if (return_data):
         return outdata
 